3342e1616d8b02ef8bcb78de7da1aefba9abd680
tulbadex <ibrahimadedayo@rocketmail.com>
2021-08-22 11:59:20 -0700

    Amend portfolio

--- action/action.php
+++ action/action.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         
             try {
                 //Server settings SMTP::DEBUG_SERVER
-                $mail->SMTPDebug = 0;     //Enable verbose debug output
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;     //Enable verbose debug output
                 $mail->isSMTP();                              //Send using SMTP
                 $mail->Host       = 'smtp.gmail.com';          //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                     //Enable SMTP authentication
